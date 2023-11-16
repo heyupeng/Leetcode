46ceb07c71e1d6200da6546d564ccf06b58f143a
peng <13750523250@163.com>
2023-11-16 15:53:07 +0800

    #30. 串联所有单词的子串 (滑动窗口）

--- LeetCodeCPlus/Solution2311.hpp
+++ LeetCodeCPlus/Solution2311.hpp
@@ -256,6 +256,115 @@ public:
         }
         return res;
     }
+    
+/// MARK: #30. 串联所有单词的子串
+    /// #30. 串联所有单词的子串
+    /// (困难)
+    ///
+    /// 给定一个字符串 s 和一个字符串数组 words。 words 中所有字符串 长度相同。
+    ///
+    ///  s 中的 串联子串 是指一个包含  words 中所有字符串以任意顺序排列连接起来的子串。
+    ///
+    /// 例如，如果 words = ["ab","cd","ef"]， 那么 "abcdef"， "abefcd"，"cdabef"， "cdefab"，"efabcd"， 和 "efcdab" 都是串联子串。 "acdbef" 不是串联子串，因为他不是任何 words 排列的连接。
+    ///
+    /// 返回所有串联子串在 s 中的开始索引。你可以以 任意顺序 返回答案。
+    ///
+    /// @code
+    /// 示例 1：
+    /// 输入：s = "barfoothefoobarman", words = ["foo","bar"]
+    /// 输出：[0,9]
+    /// 解释：因为 words.length == 2 同时 words[i].length == 3，连接的子字符串的长度必须为 6。
+    /// 子串 "barfoo" 开始位置是 0。它是 words 中以 ["bar","foo"] 顺序排列的连接。
+    /// 子串 "foobar" 开始位置是 9。它是 words 中以 ["foo","bar"] 顺序排列的连接。
+    /// 输出顺序无关紧要。返回 [9,0] 也是可以的。
+    ///
+    /// 示例 2：
+    /// 输入：s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+    /// 输出：[]
+    /// 解释：因为 words.length == 4 并且 words[i].length == 4，所以串联子串的长度必须为 16。
+    /// s 中没有子串长度为 16 并且等于 words 的任何顺序排列的连接。
+    /// 所以我们返回一个空数组。
+    ///
+    /// 示例 3：
+    /// 输入：s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+    /// 输出：[6,9,12]
+    /// 解释：因为 words.length == 3 并且 words[i].length == 3，所以串联子串的长度必须为 9。
+    /// 子串 "foobarthe" 开始位置是 6。它是 words 中以 ["foo","bar","the"] 顺序排列的连接。
+    /// 子串 "barthefoo" 开始位置是 9。它是 words 中以 ["bar","the","foo"] 顺序排列的连接。
+    /// 子串 "thefoobar" 开始位置是 12。它是 words 中以 ["the","foo","bar"] 顺序排列的连接。
+    ///
+    /// @endcode
+    ///
+    /// 提示：
+    /// - 1 <= s.length <= 104
+    /// - 1 <= words.length <= 5000
+    /// - 1 <= words[i].length <= 30
+    /// - words[i] 和 s 由小写英文字母组成
+    ///
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int n = (int)words.size();
+        int wl = (int)words[0].size();
+        int sl = (int)s.size();
+        
+        vector<int> res;
+        map<string, int> mp;
+        for (auto & word: words) {
+            mp[word] ++;
+        }
+        
+        vector<int> flags(sl, 0);
+        int cnt = n;
+        int si = 0;
+        auto cstr = s.c_str();
+        for (int k = 0; k < sl - n * wl + 1; k++) {
+            if (flags[k] != 0) continue;
+            if (cnt != n) {
+                mp.clear();
+                for (auto & word: words) {
+                    mp[word] ++;
+                }
+            }
+            cnt = n; si = k;
+            
+            for (int i = k; i < sl - wl + 1; ) {
+                if (flags[si] != 0) break;
+                
+                string sub = s.substr(i, wl);
+                if (!mp.count(sub)) {
+                    for (int j = si; j < i+1; j+=wl) flags[j] = -1;
+                    break;
+                }
+                if (mp[sub] == 0) {
+                    string ss = s.substr(si, wl);
+                    while (ss != sub) {
+                        flags[si] = -1;
+                        mp[ss] ++;
+                        cnt ++;
+                        si += wl;
+                        ss = s.substr(si, wl);
+                    }
+                    flags[si] = -1;
+                    si += wl;
+                    if (flags[si] != 0)
+                        break;
+                } else {
+                    mp[sub] --;
+                    cnt --;
+                }
+                
+                i += wl;
+                if (cnt == 0) {
+                    res.push_back(si);
+                    flags[si] = 1;
+                    sub = s.substr(si, wl);
+                    mp[sub] ++;
+                    cnt ++;
+                    si += wl;
+                }
+            }
+        }
+        return res;
+    }
 };
 
 void test_s2311();
